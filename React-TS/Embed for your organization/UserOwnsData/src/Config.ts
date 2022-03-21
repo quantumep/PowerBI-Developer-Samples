8f4619f7c54b8c9e4156e6717c89ec76ee5d68cd
william mckenzie <c1rpc@work.home.cartewright.com>
2022-03-21 09:43:19 -0500

    local changes so it can build

--- React-TS/Embed for your organization/UserOwnsData/src/Config.ts
+++ React-TS/Embed for your organization/UserOwnsData/src/Config.ts
@@ -10,10 +10,10 @@
 export const scopes: string[] = ["https://analysis.windows.net/powerbi/api/Report.Read.All"];
 
 // Client Id (Application Id) of the AAD app.
-export const clientId: string = "";
+export const clientId: string = "7fe33f8a-050c-4348-b199-2364d225b762";
 
 // Id of the workspace where the report is hosted
-export const workspaceId: string = "";
+export const workspaceId: string = "710ad5fe-7a98-45e1-acf6-260d2018aa4e";
 
 // Id of the report to be embedded
-export const reportId: string = "";
+export const reportId: string = "36957f9a-6206-496d-9f38-4516605e8c50";
